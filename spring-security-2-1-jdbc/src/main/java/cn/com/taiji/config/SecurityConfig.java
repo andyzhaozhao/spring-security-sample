924fd2f5afd8c3296d014a0e88aa59ae8b9d11f2
andy_zhaozhao@hotmail.com <andy_zhaozhao@hotmail.com>
2018-12-03 20:12:06 +0800

    Add: 完善demo

---
+++ spring-security-2-1-jdbc/src/main/java/cn/com/taiji/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package cn.com.taiji.config;//package cn.com.taiji.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+
+/**
+ * Spring Security 配置类.
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 自定义配置
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/css/**", "/js/**", "/fonts/**", "/login").permitAll()  // 都可以访问
+                .antMatchers("/h2-console/**").permitAll()  // 都可以访问
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()   //基于 Form 表单登录验证
+                .loginPage("/login").failureUrl("/login-error");
+
+        http.csrf().ignoringAntMatchers("/h2-console/**"); // 禁用 H2 控制台的 CSRF 防护
+        http.headers().frameOptions().sameOrigin(); // 允许来自同一来源的H2 控制台的请求
+    }
+
+    @Bean
+    public DataSource appDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+
+    /**
+     * 认证信息管理
+     *
+     * @param auth
+     * @throws Exception
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("123456").roles("USER")
+//                .and()
+//                .withUser("admin").password("123456").roles("USER","ADMIN");
+
+        auth.jdbcAuthentication()
+                .dataSource(appDataSource())
+                .withDefaultSchema()
+                .withUser("user").password("123456").roles("USER")
+                .and()
+                .withUser("admin").password("123456").roles("USER","ADMIN");
+    }
+}
