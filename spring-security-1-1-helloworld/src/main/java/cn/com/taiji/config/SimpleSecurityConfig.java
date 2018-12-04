082988f4030aa5eca56e6fb17e17764aeee13162
andy_zhaozhao@hotmail.com <andy_zhaozhao@hotmail.com>
2018-12-04 15:25:41 +0800

    Mod: 默认最简单配置

--- spring-security-1-1-helloworld/src/main/java/cn/com/taiji/config/SimpleSecurityConfig.java
+++ spring-security-1-1-helloworld/src/main/java/cn/com/taiji/config/SimpleSecurityConfig.java
@@ -1,12 +1,12 @@
-//package cn.com.taiji.config;
-//
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-///**
-// * Spring Security 配置类.
-// */
-//@EnableWebSecurity
-//public class SimpleSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//}
+package cn.com.taiji.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring Security 配置类.
+ */
+@EnableWebSecurity
+public class SimpleSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
