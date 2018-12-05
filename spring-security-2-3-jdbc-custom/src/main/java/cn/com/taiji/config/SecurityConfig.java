6ac182c333b139ca22b6ee958a51c03f9eefb637
andy_zhaozhao@hotmail.com <andy_zhaozhao@hotmail.com>
2018-12-05 23:51:55 +0800

    Mod: add pom

--- spring-security-2-3-jdbc-custom/src/main/java/cn/com/taiji/config/SecurityConfig.java
+++ spring-security-2-3-jdbc-custom/src/main/java/cn/com/taiji/config/SecurityConfig.java
@@ -1,60 +1,51 @@
-//package cn.com.taiji.config;//package cn.com.taiji.config;
-//
-//import cn.com.taiji.support.CustomAuthenticationProvider;
-//import cn.com.taiji.support.CustomUserDetailsService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-////import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-////import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+package cn.com.taiji.config;//package cn.com.taiji.config;
+
+import cn.com.taiji.support.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 //import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 //import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.provisioning.JdbcUserDetailsManager;
-//
-//import javax.sql.DataSource;
-//
-///**
-// * Spring Security 配置类.
-// */
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    /**
-//     * 自定义配置
-//     */
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/css/**", "/js/**", "/fonts/**", "/login").permitAll()  // 都可以访问
-//                .antMatchers("/users/**").hasRole("USER")   // 需要相应的角色才能访问
-//                .antMatchers("/admins/**").hasRole("ADMIN")   // 需要相应的角色才能访问
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()   //基于 Form 表单登录验证
-//                .loginPage("/login").failureUrl("/login-error");
-//    }
-//
-////    @Autowired
-////    private CustomAuthenticationProvider customAuthenticationProvider;
-//
+
+/**
+ * Spring Security 配置类.
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 自定义配置
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/css/**", "/js/**", "/fonts/**", "/login").permitAll()  // 都可以访问
+                .antMatchers("/users/**").hasRole("USER")   // 需要相应的角色才能访问
+                .antMatchers("/admins/**").hasRole("ADMIN")   // 需要相应的角色才能访问
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()   //基于 Form 表单登录验证
+                .loginPage("/login").failureUrl("/login-error");
+    }
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
 //    @Autowired
 //    private CustomUserDetailsService userDetailsService;
-//
-//    /**
-//     * 认证信息管理
-//     *
-//     * @param builder
-//     * @throws Exception
-//     */
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
-////        builder.authenticationProvider(customAuthenticationProvider);
+
+    /**
+     * 认证信息管理
+     *
+     * @param builder
+     * @throws Exception
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder.authenticationProvider(customAuthenticationProvider);
 //        builder.userDetailsService(userDetailsService);
-//    }
-//}
+    }
+}
